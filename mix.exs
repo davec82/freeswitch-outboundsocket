e5ceadc895aeed6bcce78a613c217d9d78f7cd08
Davide Colombo <dcolombo@voismart.it>
2017-12-27 10:56:44 +0100

    fix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,6 @@ defmodule App.Mixfile do
     [
       app: :event_socket_outbound,
       version: "0.1.0",
-      elixir: "~> 1.5",
       #build_path: "../../_build",
       #config_path: "../../config/config.exs",
       #deps_path: "../../deps",
@@ -24,15 +23,9 @@ defmodule App.Mixfile do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    []
-  end
-
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 0.8.10"},
