77f6cd5b938b82339635c6457573ad84ffde4f40
Davide Colombo <dcolombo@voismart.it>
2017-12-27 10:42:36 +0100

    add travis

--- mix.exs
+++ mix.exs
@@ -15,6 +15,7 @@ defmodule App.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      aliases: aliases(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         "coveralls": :test, "coveralls.detail": :test,
@@ -37,7 +38,26 @@ defmodule App.Mixfile do
       {:credo, "~> 0.8.10"},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
       {:ranch, "~> 1.4"},
+      {:inch_ex, "~> 0.5", only: :docs},
       {:excoveralls, "~> 0.7.5", only: :test}
     ]
+  end
+
+  defp aliases do
+    ["test.ci": &test_ci/1]
+  end
+
+  defp test_ci(args) do
+    args = if IO.ANSI.enabled?, do: ["--color"|args], else: ["--no-color"|args]
+    args = if System.get_env("TRAVIS_SECURE_ENV_VARS") == "true", do: ["--include=integration"|args], else: args
+
+    {_, res} = System.cmd("mix",
+                          ["test"|args],
+                          into: IO.binstream(:stdio, :line),
+                          env: [{"MIX_ENV", "test"}])
+
+    if res > 0 do
+      System.at_exit(fn _ -> exit({:shutdown, 1}) end)
+    end
   end
 end
